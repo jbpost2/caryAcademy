c70daccba9c4dcd05330536ebb4d26dcbe5bdce0
Justin Post <jbpost2@ncsu.edu>
2021-10-28 15:07:29 -0400

    First run of app done

--- CardSim/app.R
+++ CardSim/app.R
@@ -1,47 +1,99 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+#Create a quick app to "guess" five card suits and then plot the results
 
 library(shiny)
-
-# Define UI for application that draws a histogram
+library(DT)
 ui <- fluidPage(
+    titlePanel("Guessing Cards!"),
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
+    column(2,
+            actionButton("sample", label = "Generate 1 Guess"),
+            br(),
+            actionButton("sample10", label = "Generate 10 Guesses"),
+            br(),
+            actionButton("sample100", label = "Generate 100 Guesses")
+    ),
+    column(10,
+           fluidRow(
+            column(5,
+                   dataTableOutput("guesses")
+                   ),
+            column(7,
+                   plotOutput("dotplot")
+                   )
+           )
         )
-    )
 )
 
-# Define server logic required to draw a histogram
+#create cards
+suits <- c("D", "H", "C", "S")
+card <- pull(tidyr::unite(expand.grid(cards = c("A", 2:10, "J", "Q", "K"),
+                                      suits),
+                          col = card, sep = "-"), card)
+#helper functions for sampling
+sampleCards <- function(){sample(card, 5, replace = TRUE)}
+sampleSuits <- function(){sample(suits, 5, replace = TRUE)}
+numCorrect <- function(cards, suits){
+    sum(sub('.*-', "", cards) == suits)
+}
+
+#new row
+addOne <- function(){
+    samp1 <- sampleCards()
+    samp2 <- sampleSuits()
+    numberCorrect <- numCorrect(samp1, samp2)
+    cols <- paste0("Card", 1:5)
+    cards <- as.data.frame(t(samp1), row.names = "Cards")
+    names(cards) <- cols
+    guesses <- as.data.frame(t(samp2), row.names = "Guesses")
+    names(guesses) <- cols
+    return(list(cards = cards, guesses = guesses, numberCorrect = numberCorrect))
+}
+
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    values <- reactiveValues(
+        data = data.frame(Card1 = character(),
+                          Card2 = character(),
+                          Card3 = character(),
+                          Card4 = character(),
+                          Card5 = character(),
+                          Correct = character()
+                          )
+    )
+    
+    observeEvent(input$sample, {
+        grab <- addOne()
+        values$data <- rbind(values$data, cbind(rbind(grab$cards, grab$guesses), Correct = c(grab$numberCorrect, "")))
+   })
+    
+    observeEvent(input$sample10, {
+        replicate(10, {
+        grab <- addOne()
+        values$data <- rbind(values$data, cbind(rbind(grab$cards, grab$guesses), Correct = c(grab$numberCorrect, "")))
+        })
+    })
+    
+    observeEvent(input$sample100, {
+        replicate(100, {
+            grab <- addOne()
+            values$data <- rbind(values$data, cbind(rbind(grab$cards, grab$guesses), Correct = c(grab$numberCorrect, "")))
+        })
+    })
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+    output$guesses <- renderDataTable({
+        datatable(values$data, options = list(pageLength = 50, lengthMenu = 50))
+    })
+    
+    
+    output$dotplot <- renderPlot({
+        if(nrow(values$data) == 0){
+            
+        } else {
+            values$data$Correct %>% 
+                as.numeric() %>% 
+                hist(main = "Histogram of # Correct", xlab = "Number Correct", breaks = 0:11/2-0.25)
+        }
     })
 }
 
